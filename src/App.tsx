96e4cd14ef71f60b66c49d5c77e1d5285004e053
Brian <justbrian226@gmail.com>
2026-06-05 14:36:50 +0400

    change project data to progress data

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ const tabs: Tab[] = [
   },
   {
     key: 'dynamic',
-    label: 'Project Data',
+    label: 'Progress Data',
     icon: (
       <svg className="w-5 h-5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
